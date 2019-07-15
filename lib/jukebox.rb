49590dee1713aba3f082e98c1c377de23cf573c7
Doug Kintop <dkintop@hotmail.com>
2019-07-15 23:09:17 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -61,7 +61,19 @@ end
 =end
 
 
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_request = gets.chomp
+  songs.each_with_index do |song, i|
+    if user_request.to_i == i
+    puts "playing #{songs[i-1]}"
+  elsif user_request == song 
+  puts "playing #{song[i]}"
+else puts "Invalid input, please try again"
+  end 
+end 
 
+end 
 
 
 
