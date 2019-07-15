b967af80d9da588391f4444d97bd78e5db24d4b4
Doug Kintop <dkintop@hotmail.com>
2019-07-15 23:49:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -93,7 +93,29 @@ end
  
  
 
-  
+  def run(songs)	
+  help	
+
+   input = ""	
+  while input	
+    puts "Please enter a command:"	
+    input = gets.downcase.strip	
+    case input	
+    when 'list'	
+      list(songs)	
+    when 'play'	
+      list(songs)	
+      play(songs)	
+    when 'help'	
+      help	
+    when 'exit'	
+      exit_jukebox	
+      break	
+    else	
+      help	
+    end	
+  end	
+end
 
 
 
